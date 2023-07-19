903d5615af19952a737d013836f5743fe6becc8f
tomashina <tomislav@agmedia.hr>
2023-07-19 12:51:55 +0200

    fixes attributes list

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -219,6 +219,8 @@ class ControllerProductCategory extends Controller {
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'model'  => $result['model'],
+                    'ean'  => $result['ean'],
+                    'attribute_groups'       => $this->model_catalog_product->getProductAttributes($result['product_id']),
 					'thumb'       => $image,
 					'name'        => $result['name'],
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
