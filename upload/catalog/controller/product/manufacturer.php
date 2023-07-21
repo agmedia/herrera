c16f6fdc1c9e4b123d56f32c571b8b9a89965f8a
tomashina <tomislav@agmedia.hr>
2023-07-21 10:20:44 +0200

    fix manufacturer list

--- upload/catalog/controller/product/manufacturer.php
+++ upload/catalog/controller/product/manufacturer.php
@@ -208,6 +208,8 @@ class ControllerProductManufacturer extends Controller {
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
+                    'suplierqty'  => $result['suplierqty'],
+                    'attribute_groups'       => $this->model_catalog_product->getProductAttributes($result['product_id']),
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
