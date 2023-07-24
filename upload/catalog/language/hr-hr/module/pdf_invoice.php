13f1f6c9e131b719839979ab2ffd8c321b9f6b49
tomashina <tomislav@agmedia.hr>
2023-07-24 11:36:21 +0200

    pdf invoice

--- upload/catalog/language/hr-hr/module/pdf_invoice.php
+++ upload/catalog/language/hr-hr/module/pdf_invoice.php
@@ -1,2 +1,98 @@
 <?php
-require(DIR_SYSTEM.'../catalog/language/english/module/pdf_invoice.php');
+// Modul
+$_['text_pdf_invoice'] = 'Preuzmi PDF fakturu';
+
+// Predložak
+$_['pdf_date_format'] = '%m/%d/%Y'; // provjerite ostale formate ovdje: http://php.net/strftime, na primjer koristite '%B %d, %Y' za '1. siječnja 2016'
+$_['direction'] = 'ltr';
+$_['decimal_point'] = '.';
+$_['thousand_point'] = ',';
+$_['text_invoice'] = 'Faktura';
+$_['text_proformat'] = 'Predračun';
+$_['text_refund'] = 'Povrat novca';
+$_['text_recurring'] = 'Faktura koja se ponavlja';
+$_['text_packingslip'] = 'Otpremnica';
+$_['file_return'] = 'vrati';
+$_['text_return'] = 'Povratak';
+$_['text_store_vat'] = 'PDV ID:';
+$_['text_store_company'] = 'ID tvrtke:';
+$_['text_url'] = 'URL:';
+$_['text_company_id'] = 'ID tvrtke:';
+$_['text_tax_id'] = 'PDV ID:';
+$_['text_order_detail'] = 'Detalji o narudžbi';
+$_['text_invoice_no'] = 'Broj fakture:';
+$_['text_order_id'] = 'ID narudžbe:';
+$_['text_status'] = 'Status:';
+$_['text_date_added'] = 'Datum dodavanja:';
+$_['text_date_due'] = 'Rok dospijeća:';
+$_['text_customer'] = 'Kupac:';
+$_['text_customer_id'] = 'ID kupca:';
+$_['text_shipping_address'] = 'Adresa za dostavu';
+$_['text_shipping_method'] = 'Način dostave:';
+$_['text_payment_address'] = 'Adresa za plaćanje';
+$_['text_payment_method'] = 'Način plaćanja:';
+$_['text_products'] = 'Proizvodi:';
+$_['text_total'] = 'Ukupno:';
+$_['text_instruction'] = 'Upute';
+$_['text_customer_comment'] = 'Komentar korisnika';
+$_['text_email'] = 'E-pošta:';
+$_['text_telephone'] = 'Telefon:';
+$_['text_fax'] = 'Faks:';
+$_['text_date'] = 'Datum';
+$_['text_comment'] = 'Komentar';
+$_['text_status'] = 'Status';
+$_['text_paid'] = 'Plaćeno';
+$_['text_unpaid'] = 'Neplaćeno';
+
+// Povratak
+$_['text_return_id'] = 'ID povrata';
+$_['text_return_info'] = 'Informacije o povratu';
+$_['text_return_product'] = 'Vrati proizvod';
+$_['text_return_status'] = 'Status povrata';
+$_['text_return_reason'] = 'Razlog vraćanja';
+$_['text_return_opened'] = 'Otvoreno';
+$_['text_return_comment'] = 'Komentar';
+$_['text_return_action'] = 'Akcija vraćanja';
+
+// Ponavljajući
+$_['text_description'] = 'Opis';
+$_['text_reference'] = 'Referenca';
+$_['text_product'] = 'Proizvod';
+
+// Stupac
+$_['column_image'] = '';
+$_['column_product'] = 'Naziv proizvoda';
+$_['column_product_id'] = 'ID proizvoda';
+$_['column_model'] = 'Model';
+$_['column_manufacturer'] = 'Proizvođač';
+$_['column_description'] = 'Opis';
+$_['column_mpn'] = 'MPN';
+$_['column_isbn'] = 'ISBN';
+$_['column_location'] = 'Lokacija';
+$_['column_sku'] = 'SKU';
+$_['column_upc'] = 'UPC';
+$_['column_ean'] = 'EAN';
+$_['column_barcode'] = 'Kôd';
+$_['column_expected'] = 'Nazad naručeno (očekivano)';
+$_['column_weight'] = 'Težina';
+$_['column_attributes'] = 'Atributi';
+$_['column_quantity'] = 'Količina';
+$_['column_slip_qty'] = 'KOLIČINA Naručena';
+$_['column_qty_check'] = 'KOLIČINA poslana';
+$_['column_price'] = 'Cijena pr. porez';
+$_['column_price_tax'] = 'Cijena';
+$_['column_date_added'] = 'Datum';
+$_['column_type'] = 'Tip';
+$_['column_amount'] = 'Iznos';
+$_['column_tax'] = 'Porez';
+$_['column_special_percent'] = 'Spec./disc.';
+$_['column_tax_total'] = 'Porez (ukupno)';
+$_['column_tax_rate'] = 'Porezna stopa';
+$_['column_total'] = 'Ukupno pr. porez';
+$_['column_total_tax'] = 'Ukupno';
+$_['text_total_refund'] = 'Vraćeni iznos';
+
+$_['text_slip_summary'] = 'Sažetak pakiranja';
+$_['text_slip_total_items'] = 'Ukupan broj stavki';
+$_['text_slip_total_weight'] = 'Ukupna težina';
+?>
