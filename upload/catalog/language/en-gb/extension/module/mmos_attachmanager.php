9f7648e36bbf30b7073f40a8c8bf1c8a03112551
tomashina <tomislav@agmedia.hr>
2023-07-25 11:04:43 +0200

    product attachment

--- upload/catalog/language/en-gb/extension/module/mmos_attachmanager.php
+++ upload/catalog/language/en-gb/extension/module/mmos_attachmanager.php
@@ -1,11 +1,11 @@
 <?php
-$_['attach_button_download'] = 'Get File';
+$_['attach_button_download'] = 'Preuzmite dokument';
 $_['attach_thumb'] = 'Thumb';
-$_['attach_linkname'] = 'Link Name';
+$_['attach_linkname'] = 'Link';
 $_['attach_filename'] = 'File information';
-$_['attach_downloaded'] = 'Downloaded';
-$_['attach_filesize'] = 'File Size';
-$_['attach_action'] = 'Action';
-$_['external_link'] = 'External Links';
-$_['attach_error_login'] = 'You must login to download file';
+$_['attach_downloaded'] = 'Preuzeto';
+$_['attach_filesize'] = 'Veličina';
+$_['attach_action'] = 'Akacija';
+$_['external_link'] = 'Vanjski linkovi';
+$_['attach_error_login'] = 'Morete se prijaviti za download';
 ?>
