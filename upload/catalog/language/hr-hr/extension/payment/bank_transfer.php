2ff0006fb159d44168df8b38a2f7f9084edfef2d
tomashina <tomislav@agmedia.hr>
2024-05-07 11:21:31 +0200

    customer form

--- upload/catalog/language/hr-hr/extension/payment/bank_transfer.php
+++ upload/catalog/language/hr-hr/extension/payment/bank_transfer.php
@@ -4,4 +4,4 @@
 $_['text_title']				= 'Bankovna transakcija (internet bankarstvo)';
 $_['text_instruction']			= 'Upute za bankovnu transakciju';
 $_['text_description']			= 'Molimo prebacite ukupni iznos za naplatu na slijedeći bankovni račun.';
-$_['text_payment']				= 'Vaša narudžba neće biti isporučena dok uplata ne sjedne na naš račun.';
+$_['text_payment']				= '';
