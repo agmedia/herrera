de6ccd513399765e30e7d639e73a57bec015105c
tomashina <tomislav@agmedia.hr>
2024-06-17 12:41:45 +0200

    customer approval

---
+++ upload/admin/language/hr-hr/customer/customer_approval.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']         = 'Odobrenja Kupaca';
+
+// Tekst
+$_['text_success']          = 'Uspjeh: Izmijenili ste odobrenja kupaca!';
+$_['text_list']             = 'Popis Odobrenja Kupaca';
+$_['text_default']          = 'Zadano';
+$_['text_customer']         = 'Kupac';
+$_['text_affiliate']        = 'Partner';
+
+// Stupac
+$_['column_name']           = 'Ime Kupca';
+$_['column_email']          = 'E-Mail';
+$_['column_customer_group'] = 'Grupa Kupaca';
+$_['column_type']           = 'Tip';
+$_['column_date_added']     = 'Datum Dodano';
+$_['column_action']         = 'Akcija';
+
+// Unos
+$_['entry_name']            = 'Ime Kupca';
+$_['entry_email']           = 'E-Mail';
+$_['entry_customer_group']  = 'Grupa Kupaca';
+$_['entry_type']            = 'Tip';
+$_['entry_date_added']      = 'Datum Dodano';
+
+// Greška
+$_['error_permission']      = 'Upozorenje: Nemate dopuštenje za izmjenu odobrenja kupaca!';
---
+++ upload/admin/language/hr-hr/mail/customer_approve.php
@@ -0,0 +1,25 @@
+<?php
+// Text
+$_['text_subject'] = '%s - Vaš račun je aktiviran!';
+$_['text_welcome'] = '<p>Poštovani,</p>					
+						
+<p>Srdačno Vas pozdravljamo i zahvaljujemo Vam na registraciji u našem B2B web shopu www.herrera.hr	</p>					
+						
+<p>Vaš račun je aktivan. 		</p>				
+						
+<p>Kako bismo Vam osigurali najbolje moguće iskustvo, željeli bismo Vas upoznati s nekoliko ključnih informacija:	</p>					
+						
+<p>1. B2B Login: za pregled cijena, ekskluzivnih ponuda ili zalihe, molimo Vas da se prijavite sa login podacima koje ste koristili prilikom registracije. U slucaju zaboravljene lozinke, unesite Vašu e-mail adresu na mjestu za prijavu, te kliknite na sekciju “Zaboravljena lozinka”. Nakon toga ćete primiti e-mail sa detaljnim uputama za promjenu lozinke.			</p>			
+										
+<p>2. Moj korisnički račun: Odmah nakon prijave, imate pristup Vašem osobnom računu gdje možete pregledavati prethodne narudžbe, prikazati sve kupljene proizvode, pratiti status trenutnih narudžbi i upravljati Vašim podacima.	</p>					
+<p>3. Ekskluzivne Ponude: Kao registrirani korisnik, imat ćete pristup ekskluzivnim ponudama, popustima i promocijama koje su dostupne samo našim B2B klijentima.		</p>				
+<p>4. Podrška: Naš tim za korisničku podršku stoji vam na raspolaganju za sve upite ili pomoć pri naručivanju. Možete nas kontaktirati putem e-maila na distribution@herrera.hr ili telefona na +385 99 688 3456	</p>					
+<p>5. Proizvodi i Usluge: Nudimo širok asortiman rasvjete I elektromaterijala koje kontinuirano dopunjavamo i prilagođavamo kako bismo zadovoljili sve Vaše poslovne potrebe.		</p>				
+<p>6. Plaćanja i Dostava: Informacije o opcijama plaćanja i dostave možete pronaći putem poveznice https://www.herrera.hr/nacin-placanja-i-dostava ili kontaktirajući našu korisničku podršku.	</p>					
+						
+<p>Za sve dodatne informacije, molimo Vas da nas kontaktirate direktno. Radujemo se mogućnosti da Vam pomognemo u ostvarivanju Vaših poslovnih ciljeva.</p>						
+						
+<p>Zahvaljujemo Vam još jednom na povjerenju, te se radujemo uspješnoj suradnji!	</p>		';
+$_['text_login']   = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
+$_['text_service'] = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
+$_['text_thanks']  = 'Thanks,';
