7a9530555849024d67473920a3f91fa57487ead8
tomashina <tomislav@agmedia.hr>
2024-06-17 13:02:52 +0200

    customer approval

--- upload/catalog/language/hr-hr/mail/customer.php
+++ upload/catalog/language/hr-hr/mail/customer.php
@@ -1,10 +1,23 @@
 <?php
 // Croatian   v.2.x.x     Datum: 01.10.2014		Author: Gigo (Igor Ilić - igor@iligsoft.hr)
 // Text
-$_['text_subject']        = '%s - Zahvaljujemo se na registraciji';
+$_['text_subject']        = '%s - Dobrodošlica i potvrda registracije';
 $_['text_welcome']        = 'Dobrodošli i zahvaljujemo se za registraciju na %s!';
 $_['text_login']          = 'Vaš korisnički račun je kreiran i možete se prijaviti (logirati) s njim na našu internet trgovinu koristeći Vaš e-mail i lozinku ili prateći slijedeći URL (link):';
-$_['text_approval']       = 'Vaš korisnički račun mora biti odobren prije nego se možete prijaviti (logirati). Kad Vam korisnički račun bude odobren, možete se prijaviti (logirati) na našu internet trgovinu koristeći Vašu e-mail adresu i lozinku ili prateći slijedeći URL (link):';
+
+$_['text_approval']       = 'Poštovani/a [Ime],		
+		
+Hvala vam što ste se registrirali na Herrera – veleprodaju rasvjete i elektromaterijala! Vaša registracija je zaprimljena i trenutno se provjerava.		
+		
+Molimo vas da pričekate odobrenje vašeg korisničkog računa prije nego što se možete prijaviti na našu b2b platformu. Kada vaš korisnički račun bude odobren, moći ćete pristupiti svojem računu i koristiti naše usluge.		
+		
+U međuvremenu, ako imate bilo kakvih pitanja ili nedoumica, slobodno nas kontaktirajte putem e-maila na shop@herrera.hr ili telefonski na 0996883456.		
+		
+Srdačno,	';
+
+
+
+
 $_['text_services']       = 'Nakon prijave (logiranja), bit ćete u mogućnosti pristupati i drugim uslugama uključujući preglede prošlih narudžbi, ispise računa i ažuriranje Vaših informacija o korisničkom računu.';
 $_['text_thanks']         = 'Zahvaljujemo,';
 $_['text_new_customer']   = 'Novi kupac';
